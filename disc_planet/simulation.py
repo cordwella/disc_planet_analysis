Fix horseshoe calculation and vphi for 3D

disc_planet/simulation.py
@@ -12,13 +12,13 @@ logger = logging.getLogger(__name__)
 
 class Simulation(object):
 	"""
-	SimulationObject 
+	SimulationObject
 
 	Abstract base class that all other references should inherit from.
 	Inheriting objects should provide an __init__ function that can
 	read in data from a specific format/set of simulation settings
 
-	The format for v_R, v_phi, v_theta dens etc is a 3D numpy array 
+	The format for v_R, v_phi, v_theta dens etc is a 3D numpy array
 	where the first index is along R, the second along phi, and the third
 	along theta.
 
@@ -38,11 +38,11 @@ class Simulation(object):
 	horseshoe_max_iter	= 100
 
 	gap_analytic_ignore_outer_percent   = 0.005
-	gap_analytic_ignore_coorbital_width = 0 # in code units
+	gap_analytic_ignore_coorbital_width = 0.05 # in code units
 
 	setup = {}
 	orbit_id = None
-	potential	= None 
+	potential	= None
 	potential_2D = None
 
 
@@ -54,14 +54,14 @@ class Simulation(object):
 	R_cylindrical = None
 
 	density = None
-	v_R = None 
-	v_phi = None 
+	v_R = None
+	v_phi = None
 	v_theta = None
 
 	surface_density = None
 	vortensity = None
 	v_R_2D = None
-	v_phi_2D = None 
+	v_phi_2D = None
 
 	surface_density_1D = None
 	v_R_1D = None
@@ -70,45 +70,46 @@ class Simulation(object):
 	dsigmadt_2D = None
 	dsigmadt = None
 
+	summary = None
+
 	def __init__(self, *args, **kwargs):
 		"""
-		Should only be called via super().__init__(*args, **kwargs) in 
-		inheriting classes. Grid, velocities and density should be set 
+		Should only be called via super().__init__(*args, **kwargs) in
+		inheriting classes. Grid, velocities and density should be set
 		by the inheriting class.
 
 		Inputs (all optional):
 		- output_folder: string, where to save output data. Optional
-		  only if self.folder is set by the inheriting class 
-		- integration_method: string, 'spherical' or 'column'. For 
-		  3D spherical simulations either integrate on shells 
+		  only if self.folder is set by the inheriting class
+		- integration_method: string, 'spherical' or 'column'. For
+		  3D spherical simulations either integrate on shells
 		  ('spherical', reccomended) or interpolate and integate on
-		  columns ('column', more correct but not reccomended due 
+		  columns ('column', more correct but not reccomended due
 		  to speed issues). Default: 'spherical'
-		- include_vortensity: boolean. Default: False. Whether or 
+		- include_vortensity: boolean. Default: False. Whether or
 		  not to calculate vortensity/PV
 		- use_2d_vortensity: Boolean. Default: False. For 3D simulations
-		  whether to instead use the classical 2D definition of 
+		  whether to instead use the classical 2D definition of
 		  vortensity. (Irrelevant for 2D simulations)
-		- include_horseshoe: boolean. Default False. 
+		- include_horseshoe: boolean. Default False.
 		- horseshoe_max_width: float. Default 0.2. Maximum width,
-		  in units of R_p, that the horseshoe orbits should have. 
-		- horseshoe_del_R: float. Radial step each attempt to find the 
+		  in units of R_p, that the horseshoe orbits should have.
+		- horseshoe_del_R: float. Radial step each attempt to find the
 		  horseshoe orbit.
 		- horseshoe_tmax: float. Default 1000. In the horseshoe calculation
-		  algorithm set tmax for solve_ivp. 
-		- horseshoe_max_iter: int. Default 100. Allow how many attempts 
-		  at the horseshoe calculation algorithm. (Should go up as delR 
+		  algorithm set tmax for solve_ivp.
+		- horseshoe_max_iter: int. Default 100. Allow how many attempts
+		  at the horseshoe calculation algorithm. (Should go up as delR
 		  goes down).
-		- gap_smallest_distance: float. default H_0 * 4. Longest distance 
+		- gap_smallest_distance: float. default H_0 * 4. Longest distance
 		  away from the planet to attempt to find the first gap
-		- gap_smallest_distance: float. default 0. Shortest distance 
+		- gap_smallest_distance: float. default 0. Shortest distance
 		  away from the planet to attempt to find the gap
-		- gap_analytic_ignore_coorbital_width: float, default 0. When calculating 
-		  the analytic gap structure set f_dep to zero within this distance 
-		  away from the planet (useful for removing the effects of the unsaturated 
-		  co-rotation torque)
-		- gap_analytic_ignore_outer_percent: float. default 0.005. When 
-		  calculating the outer/inner gap set the outer and inner 
+		- gap_analytic_ignore_coorbital_width: float, default 0.03. Area
+		  around the planet to ignore in f_dep for dsigmadt_shockonly
+		  and dsigmadt_shockonly_2D
+		- gap_analytic_ignore_outer_percent: float. default 0.005. When
+		  calculating the outer/inner gap set the outer and inner
 		  gap_analytic_ignore_outer_percent of f_dep to zero. (This is
 		  to account for numerical wobbles at the edge of the domain.)
 		"""
@@ -130,13 +131,13 @@ class Simulation(object):
 		self.horseshoe_max_width = kwargs.get('horseshoe_max_width', self.horseshoe_max_width)
 		self.setup['R_p'] = self.setup.get('R_p', 1)
 		r_p_i = np.argmin(np.abs(self.R - self.setup['R_p']))
-		self.horseshoe_del_R = kwargs.get('horseshoe_del_R', (self.R[r_p_i + 1] - self.R[r_p_i]))
+		self.horseshoe_del_R = kwargs.get('horseshoe_del_R', self.R[r_p_i + 1] - self.R[r_p_i]))
 		self.horseshoe_tmax  = kwargs.get('horseshoe_tmax', self.horseshoe_tmax)
 		self.horseshoe_max_iter  = kwargs.get('horseshoe_max_iter', self.horseshoe_max_iter)
 
 		# Furthest distance away from R_p that we expect the gap to be
 		self.gap_furthest_distance = kwargs.get('gap_furthest_distance', self.setup['H0'] * 3)
-	
+
 		# Smallest distance away from the planet we expect the gap to be
 		self.gap_smallest_distance = kwargs.get('gap_smallest_distance', 0)
 
@@ -152,11 +153,11 @@ class Simulation(object):
 
 		if include_vortensity or self.include_vortensity:
 			self.calculate_vortensity()
-		
+
 		self.process_1d_outputs(*args, **kwargs)
 		return self.process_summary_outputs(*args, **kwargs)
 
-	def process_1d_outputs(self, *args, inclde_vortensity=False,
+	def process_1d_outputs(self, *args, include_vortensity=False,
 						   **kwargs):
 		# Depends on process all details having been calculated
 		if self.surface_density is None:
@@ -171,9 +172,9 @@ class Simulation(object):
 
 		if self.dimension == 3:
 			# Calculate dT/dR
-			integrand = -1 * (self.density * self.R[:, None, None] * 
+			integrand = -1 * (self.density * self.R[:, None, None] *
 				self.potential.calculate_dPhidphi_3D(self.R, self.phi, self.theta))
-			
+
 			self.dTdR = np.trapezoid(
 				self.intergrate_in_z(integrand),
 				self.phi, axis=1)
@@ -181,8 +182,8 @@ class Simulation(object):
 
 			# Calculate F_{wave}
 			d_v_phi = self.v_phi - self.v_phi_1D[:, None, None]
-			self.F_wave = self.R**2 * (np.trapezoid( 
-				self.intergrate_in_z(self.density * self.v_R * (self.v_phi - self.v_phi_1D[:, None, None])), 
+			self.F_wave = self.R**2 * (np.trapezoid(
+				self.intergrate_in_z(self.density * self.v_R * (self.v_phi - self.v_phi_1D[:, None, None])),
 				axis=1, x=self.phi))
 			self.dF_wavedR = np.gradient(self.F_wave, self.R)
 
@@ -193,15 +194,21 @@ class Simulation(object):
 			f_dep_to_use  = self.dF_depdR/self.surface_density_1D
 			f_dep_to_use[self.R < self.R[0] * (1 + self.gap_analytic_ignore_outer_percent)] = 0
 			f_dep_to_use[self.R > self.R[-1] * (1 - self.gap_analytic_ignore_outer_percent)] = 0
-			f_dep_to_use[np.abs(self.R - self.setup['R_p']) < self.gap_analytic_ignore_coorbital_width] = 0
 
 			self.dsigmadt = non_local_change_in_density(
 				self.R/self.setup['R0'], -1 * self.setup['surface_density_slope'], -1 *self.setup['temperature_slope'], self.setup['H0']/self.setup['R0'],
 				f_dep_to_use)
 
+			f_dep_to_use[np.abs(self.R - self.setup['R_p']) < self.gap_analytic_ignore_coorbital_width] = 0
+
+			self.dsigmadt_shock_only = non_local_change_in_density(
+				self.R/self.setup['R0'], -1 * self.setup['surface_density_slope'], -1 *self.setup['temperature_slope'], self.setup['H0']/self.setup['R0'],
+				f_dep_to_use)
+
+
 		# Calculate dT/dR_2D
 		self.dTdR_2D = -1 * np.trapezoid(
-			self.surface_density * self.R[:, None] * 
+			self.surface_density * self.R[:, None] *
 			self.potential_2D.calculate_dPhidphi_2D(self.R, self.phi),
 			self.phi, axis=1)
 
@@ -222,12 +229,16 @@ class Simulation(object):
 		f_dep_to_use[self.R < self.R[0] * (1 + self.gap_analytic_ignore_outer_percent)] = 0
 		f_dep_to_use[self.R > self.R[-1] * (1 - self.gap_analytic_ignore_outer_percent)] = 0
 
-		f_dep_to_use[np.abs(self.R - self.setup['R_p']) < self.gap_analytic_ignore_coorbital_width] = 0
-
-
 		self.dsigmadt_2D = non_local_change_in_density(
 			self.R/self.setup['R0'], -1 * self.setup['surface_density_slope'], -1 *self.setup['temperature_slope'], self.setup['H0']/self.setup['R0'],
 			f_dep_to_use)
+
+		f_dep_to_use[np.abs(self.R - self.setup['R_p']) < self.gap_analytic_ignore_coorbital_width] = 0
+		self.dsigmadt_shock_only_2D = non_local_change_in_density(
+			self.R/self.setup['R0'], -1 * self.setup['surface_density_slope'], -1 *self.setup['temperature_slope'], self.setup['H0']/self.setup['R0'],
+			f_dep_to_use)
+
+
 
 		if self.include_vortensity or include_vortensity:
 			if self.vortensity is None:
@@ -241,20 +252,20 @@ class Simulation(object):
 		if self.dsigmadt_2D is None:
 			self.process_1d_outputs()
 
-		gap_timescale = -1/self.dsigmadt_2D[np.argmin( np.abs(self.R - self.setup['R_p']) )]
+		gap_timescale = -1/self.dsigmadt_shock_only_2D[np.argmin( np.abs(self.R - self.setup['R_p']) )]
 		r_inner = self.R[np.logical_and(self.R < self.setup['R_p'] - self.gap_smallest_distance, self.R > (self.setup['R_p'] - self.gap_furthest_distance))]
 		r_outer = self.R[np.logical_and(self.R > self.setup['R_p'] + self.gap_smallest_distance, self.R < (self.setup['R_p'] + self.gap_furthest_distance))]
-		
+
 		inner_gap_mask = np.logical_and(self.R < self.setup['R_p'] - self.gap_smallest_distance, self.R > (self.setup['R_p'] - self.gap_furthest_distance))
 		outer_gap_mask = np.logical_and(self.R > self.setup['R_p'] + self.gap_smallest_distance, self.R < (self.setup['R_p'] + self.gap_furthest_distance))
-		r_id_inner_gap = np.argmin(self.dsigmadt_2D[inner_gap_mask])
-		r_id_outer_gap = np.argmin(self.dsigmadt_2D[outer_gap_mask])
+		r_id_inner_gap = np.argmin(self.dsigmadt_shock_only_2D[inner_gap_mask])
+		r_id_outer_gap = np.argmin(self.dsigmadt_shock_only_2D[outer_gap_mask])
 
 		gap_inner_loc = self.R[inner_gap_mask][r_id_inner_gap]
 		gap_outer_loc = self.R[outer_gap_mask][r_id_outer_gap]
-		
-		gap_inner_depth = self.dsigmadt_2D[inner_gap_mask][r_id_inner_gap]
-		gap_outer_depth = self.dsigmadt_2D[outer_gap_mask][r_id_outer_gap]
+
+		gap_inner_depth = self.dsigmadt_shock_only_2D[inner_gap_mask][r_id_inner_gap]
+		gap_outer_depth = self.dsigmadt_shock_only_2D[outer_gap_mask][r_id_outer_gap]
 
 		self.setup['2D_potential_type'] = self.potential_2D.__class__.__name__
 		self.setup['potential_type'] = self.potential.__class__.__name__
@@ -274,32 +285,32 @@ class Simulation(object):
 			'gap_inner_timescale_2D': -1/gap_inner_depth,
 			'gap_outer_timescale_2D': -1/gap_outer_depth,
 		}
-		
+
 		self.summary['osl_torque_2D'] = (self.summary['inner_torque_2D'] + self.summary['outer_torque_2D'])/2
 
 		if self.dimension == 3:
 			self.summary['total_torque'] = self.T[-1]
 			self.summary['inner_torque'] =   np.abs(np.trapz(self.dTdR[self.R < self.setup['R_p']], self.R[self.R < self.setup['R_p']]))
 			self.summary['outer_torque'] = np.abs(np.trapz(self.dTdR[self.R > self.setup['R_p']], self.R[self.R > self.setup['R_p']]))
-	
+
 			self.summary['osl_torque'] = (self.summary['inner_torque'] + self.summary['outer_torque'])/2
 
 			# Gap properties
 
-			gap_timescale = 1/self.dsigmadt[np.argmin( np.abs(self.R - self.setup['R_p']) )]
+			gap_timescale = 1/self.dsigmadt_shock_only[np.argmin( np.abs(self.R - self.setup['R_p']) )]
 			r_inner = self.R[np.logical_and(self.R < self.setup['R_p'], self.R > (self.setup['R_p'] - self.gap_furthest_distance))]
 			r_outer = self.R[np.logical_and(self.R > self.setup['R_p'], self.R < (self.setup['R_p'] + self.gap_furthest_distance))]
-			
+
 			inner_gap_mask = np.logical_and(self.R < self.setup['R_p'] - self.gap_smallest_distance, self.R > (self.setup['R_p'] - self.gap_furthest_distance))
 			outer_gap_mask = np.logical_and(self.R > self.setup['R_p'] + self.gap_smallest_distance, self.R < (self.setup['R_p'] + self.gap_furthest_distance))
-			r_id_inner_gap = np.argmin(self.dsigmadt[inner_gap_mask])
-			r_id_outer_gap = np.argmin(self.dsigmadt[outer_gap_mask])
+			r_id_inner_gap = np.argmin(self.dsigmadt_shock_only[inner_gap_mask])
+			r_id_outer_gap = np.argmin(self.dsigmadt_shock_only[outer_gap_mask])
 
 			gap_inner_loc = self.R[inner_gap_mask][r_id_inner_gap]
 			gap_outer_loc = self.R[outer_gap_mask][r_id_outer_gap]
-			
-			gap_inner_depth = self.dsigmadt[inner_gap_mask][r_id_inner_gap]
-			gap_outer_depth = self.dsigmadt[outer_gap_mask][r_id_outer_gap]
+
+			gap_inner_depth = self.dsigmadt_shock_only[inner_gap_mask][r_id_inner_gap]
+			gap_outer_depth = self.dsigmadt_shock_only[outer_gap_mask][r_id_outer_gap]
 
 			self.summary['gap_inner_loc'] =  gap_inner_loc
 			self.summary['gap_outer_loc'] =  gap_outer_loc
@@ -311,7 +322,7 @@ class Simulation(object):
 		if self.include_horseshoe or include_horseshoe:
 			# TODO: Need to have horseshoe configuration options
 			hs_width, hs_outer, hs_inner = get_horseshoe_width(
-				self.R, self.phi, 
+				self.R, self.phi,
 				self.v_R_2D, self.v_phi_2D,
 				delR = self.horseshoe_del_R,
 				tmax = self.horseshoe_tmax,
@@ -319,7 +330,7 @@ class Simulation(object):
 				rmax = (1 + self.horseshoe_max_width/2) * self.setup['R_p'],
 				rmin = (1 + self.horseshoe_max_width/2) * self.setup['R_p'])
 
-			self.summary['horseshoe_width']		= hs_width	   * self.setup['R_p']
+			self.summary['horseshoe_width']		= hs_width * self.setup['R_p']
 			self.summary['horseshoe_inner_loc']	= hs_inner * self.setup['R_p']
 			self.summary['horseshoe_outer_loc']	= hs_outer * self.setup['R_p']
 
@@ -338,7 +349,7 @@ class Simulation(object):
 
 			azimuthal_velocity = self.v_phi * self.R[:, None, None]/r_cyl
 
-			zeta_z = (np.gradient(azimuthal_velocity * self.r_cyl[:, None, None], self.R, axis=0) 
+			zeta_z = (np.gradient(azimuthal_velocity * self.r_cyl[:, None, None], self.R, axis=0)
 					  - np.gradient(r_cyl_velocity, self.phi, axis=1))/self.r_cyl/self.density
 
 			self.vortensity = self.intergrate_in_z(1/zeta_z)**(-1)
@@ -363,8 +374,8 @@ class Simulation(object):
 		return output
 
 	def intergrate_along_actual_column(self, integrand):
-		# NOTE: THIS METHOD IS VERY SLOW! 
-		# I DON'T RECCOMEND ACTUALLY USING IT WITH THE EXCEPTION OF TESTS TO ENSURE 
+		# NOTE: THIS METHOD IS VERY SLOW!
+		# I DON'T RECCOMEND ACTUALLY USING IT WITH THE EXCEPTION OF TESTS TO ENSURE
 		# THAT THE SMALL ANGLE APPROXIMATION IS VALID ENOUGH FOR YOUR APPLICATION
 
 		if self.z is None:
@@ -372,24 +383,24 @@ class Simulation(object):
 
 		if self.R_cylindrical is None:
 			self.R_cylindrical = self.R[:, None] * np.sin(self.theta[None, :])
-		
+
 		R = self.R_cylindrical
 		output = np.zeros((self.R.size, self.phi.size))
 
 		for phi_i in range(self.phi.shape[0]):
-			# construct the interpolator in spherical coordinates		
-			interp	= RegularGridInterpolator((self.R, self.theta), 
+			# construct the interpolator in spherical coordinates
+			interp	= RegularGridInterpolator((self.R, self.theta),
 				integrand[:, phi_i, :], fill_value=0, bounds_error=False)
-			
+
 			# Now for each possible R we need to calculate this seperately
 			# this is a very slow approach but I also have a very slow brain
 			for r_i in range(self.R.shape[0]):
 				r_current = self.R[r_i]
-				z_interior = -1 * np.linspace(np.max(self.z[r_i, :]), np.min(self.z[r_i, :]), 
+				z_interior = -1 * np.linspace(np.max(self.z[r_i, :]), np.min(self.z[r_i, :]),
 					self.theta.shape[0])
-				
+
 				r_sph_current = np.sqrt(r_current**2 + z_interior**2)
-				
+
 				theta_reset = np.acos(z_interior/r_sph_current)
 				a = interp(np.swapaxes(np.vstack([r_sph_current, theta_reset]), 0, 1))
 				output[r_i, phi_i] = np.trapezoid(a, z_interior, axis=0)
@@ -408,15 +419,15 @@ class Simulation(object):
 
 		r_cyl_velocity = (
 			self.v_R * np.sin(self.theta[None, None, :]) +
-			self.R[:, None, None] * np.cos(self.theta[None, None, :]) * self.v_phi)
+			self.R[:, None, None] * np.cos(self.theta[None, None, :]) * self.v_theta)
 
 		r_cyl = self.R[:, None, None] * np.sin(self.theta[None, None, :])
 
-		azimuthal_velocity = self.v_phi * self.R[:, None, None]/r_cyl
+		azimuthal_velocity = self.v_phi * np.sin(self.theta[None, None, :])
 
-		# Small angle approximation, don't bother converting 
-		self.v_R_2D   = self.intergrate_in_z(r_cyl_velocity   * self.density)/self.surface_density
-		self.v_phi_2D = self.intergrate_in_z(azimuthal_velocity * self.density)/self.surface_density
+		# Small angle approximation, don't bother converting
+		self.v_R_2D   = self.intergrate_in_z(r_cyl_velocity * self.density)/self.surface_density
+		self.v_phi_2D = self.intergrate_in_z(self.v_phi * self.density)/self.surface_density
 
 		if self.include_vortensity:
 			self.calculate_vortensity()
@@ -428,16 +439,17 @@ class Simulation(object):
 
 		keys = ['setup',
 				'R',
-				'time', 
+				'time',
 				'dsigmadt_2D',
+				'dsigmadt_shock_only_2D',
 				'surface_density_1D',
 				'v_R_1D',
 				'v_phi_1D',
 				'dTdR_2D',
 				'T_2D' ,
-				'F_wave_2D', 
-				'dF_wavedR_2D', 
-				'F_dep_2D', 
+				'F_wave_2D',
+				'dF_wavedR_2D',
+				'F_dep_2D',
 				'dF_depdR_2D',
 				'surface_density',
 				'v_R_2D',
@@ -446,7 +458,7 @@ class Simulation(object):
 
 		# add in the 2D modes of torque calculations
 		if self.dimension == 3:
-			keys = keys + ['dsigmadt', 'dTdR', 'F_wave',
+			keys = keys + ['dsigmadt', 'dsigmadt_shock_only', 'dTdR', 'F_wave',
 						   'dF_wavedR', 'F_dep', 'dF_depdR']
 
 
@@ -459,8 +471,8 @@ class Simulation(object):
 		keys = ['setup',
 				'R',
 				'phi',
-				'time', 
-				'setup', 
+				'time',
+				'setup',
 				'surface_density',
 				'v_R_2D',
 				'v_phi_2D']
@@ -477,23 +489,27 @@ class Simulation(object):
 
 		keys = ['setup',
 				'R',
-				'time', 
+				'time',
 				'dsigmadt_2D',
+				'dsigmadt_shock_only_2D',
 				'surface_density_1D',
 				'v_R_1D',
 				'v_phi_1D',
 				'dTdR_2D',
 				'T_2D' ,
-				'F_wave_2D', 
-				'dF_wavedR_2D', 
-				'F_dep_2D', 
-				'dF_depdR_2D',
-				'summary']
+				'F_wave_2D',
+				'dF_wavedR_2D',
+				'F_dep_2D',
+				'dF_depdR_2D']
+
+		if self.summary:
+			keys.append('summary')
 
 		# add in the 2D modes of torque calculations
 		if self.dimension == 3:
-			keys = keys + ['dsigmadt', 'dTdR', 'F_wave',
-						   'dF_wavedR', 'F_dep', 'dF_depdR']
+			keys = keys + ['dsigmadt', 'dsigmadt_shock_only',
+					 'dTdR', 'F_wave',
+					'dF_wavedR', 'F_dep', 'dF_depdR']
 		if self.include_vortensity:
 			keys.append('vortensity_1D')
 
@@ -517,15 +533,15 @@ class ProcessedData(Simulation):
 		'setup',
 		'R',
 		'phi',
-		'time', 
+		'time',
 		'dsigmadt_2D',
 		'surface_density_1D',
 		'v_R_1D',
 		'v_phi_1D',
-		'dTdR', 
-		'F_wave', 
-		'dF_wavedR', 
-		'F_dep', 
+		'dTdR',
+		'F_wave',
+		'dF_wavedR',
+		'F_dep',
 		'dF_depdR',
 		'dTdR_2D', 'F_wave_2D', 'dF_wavedR_2D', 'F_dep_2D', 'dF_depdR_2D',
 		'surface_density',
@@ -534,7 +550,7 @@ class ProcessedData(Simulation):
 	]
 
 
-	def __init__(self, filename, *args, **kwargs):		
+	def __init__(self, filename, *args, **kwargs):
 		file = open(filename, 'rb')
 		data = pickle.load(file)
 		file.close()

disc_planet/utils.py
@@ -5,29 +5,29 @@ from scipy.integrate import cumulative_trapezoid, solve_ivp
 from scipy.special import iv, ive, kv, kve
 from scipy.interpolate import RegularGridInterpolator
 
-# Implements the analytic solutions for the initial evolution of a gap in a 
-# protoplanetary disc as in Cordwell & Rafikov 2024 
+# Implements the analytic solutions for the initial evolution of a gap in a
+# protoplanetary disc as in Cordwell & Rafikov 2024
 
 logger = logging.getLogger(__name__)
 
 def non_local_change_in_density(R, p, q, h, f_dep):
     """
-    Global solution for the initial evolution of a gap in a 
+    Global solution for the initial evolution of a gap in a
     protoplanetary disc.
-    
+
     See equation C4 in Cordwell & Rafikov (2024)
-    
+
     Inputs:
     - R, input array of locations to evaluate
     - p, background gradient in surface density (Sigma_0 = R^(-p))
     - q, background gradient in disc temperature (c_s = c_{s, p} R^(-q) )
     - h, scale height of the disc at $R = 1$
     - f_dep, pre-evaluated angular momentum deposition function. Defined as $\partial \F_{dep}/\partial R / \Sigma$
-    
+
     Returns:
     - d\sigma / dt, array with the same shape as R
     """
-    
+
     if q == 0.5:
         logger.warning('Approximating q == 0.5 as q = 0.55')
         q = 0.55
@@ -36,22 +36,22 @@ def non_local_change_in_density(R, p, q, h, f_dep):
     A = h
 
     S = 1/np.pi * np.gradient(R**(1/2 - p) * f_dep)/np.gradient(R)
-    
+
     x = R**(q - 1/2)/( (q - 1/2) * A)
     x = x.astype(complex)
-    
+
     beta = np.abs((p + 2 * q - 3)/(1 - 2 * q))
 
     I_3 = iv(beta, np.abs(x))
     K_3 = kv(beta, np.abs(x))
-    
+
     if q < 1/2:
         i_int = cumulative_trapezoid((I_3 * S * R**(p/2 + q - 3/2))[::-1], R[::-1], initial=0)[::-1]
         k_int = cumulative_trapezoid((K_3 * S * R**(p/2 + q - 3/2)), R, initial=0)
     else:
         i_int = cumulative_trapezoid((I_3 * S * R**(p/2 + q - 3/2)), R, initial=0)
         k_int = cumulative_trapezoid((K_3 * S * R**(p/2 + q - 3/2))[::-1], R[::-1], initial=0)[::-1]
-    
+
     return A **(-2) * R**(p/2 + q - 3/2) * (
         I_3 * k_int - K_3 * i_int)/(q - 1/2)
 
@@ -68,13 +68,14 @@ def get_horseshoe_width(R, phi, vr, vphi,
                         max_step = 5,
                         background_vphi = None):
 
+    delR = 0.0005
     if background_vphi is None:
         background_vphi = R[:, None]**(3/2)
     interp_vr    = RegularGridInterpolator((phi, R), np.swapaxes(vr, 0, 1), bounds_error=False, fill_value=0)
     interp_vphi  = RegularGridInterpolator((phi, R), np.swapaxes(vphi - background_vphi, 0 , 1), bounds_error=False, fill_value=0)
 
 
-    def hit_bounds_phi(t, Y): 
+    def hit_bounds_phi(t, Y):
         # Check if it has hit the bounds of the domain in phi
         a = min(5 - Y[0], Y[0] - phi_min * 1.01)
 
@@ -83,14 +84,14 @@ def get_horseshoe_width(R, phi, vr, vphi,
         return a
 
 
-    def hit_bounds_r(t, Y): 
+    def hit_bounds_r(t, Y):
         # Check if it has hit the bounds of the domain in R
         return min(rmax * 0.99 - Y[1], Y[1] - rmin * 1.01)
 
     # Exit at the edge of bounds
     hit_bounds_r.terminal = True
     hit_bounds_phi.terminal = True
-    
+
     def velocity(t, y):
         return np.squeeze([interp_vphi(y), interp_vr(y)])
 
@@ -116,7 +117,7 @@ def get_horseshoe_width(R, phi, vr, vphi,
             return 0, 0, 0
 
         # Have we escaped the horseshoe region?
-        final_position_r = sol.y[1][-1] 
+        final_position_r = sol.y[1][-1]
         final_position_phi = sol.y[0][-1]
 
         if final_position_phi > np.pi * 1.1: